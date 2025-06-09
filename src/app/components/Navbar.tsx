ae79fa2a0764768a4c508d5fcc6d1c41d28e6314
Youssef25331 <yousseft25331@gmail.com>
2025-06-09 19:59:18 +0300

    navbar setup

---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className="navbar-center bg-neutral px-2 py-1.5 mt-9 rounded-4xl">
+      <button className="btn rounded btn-ghost text-sm font-normal">Home</button>
+      <button className="btn btn-ghost text-sm font-normal">Home</button>
+      <button className="btn btn-ghost text-sm font-normal">Home</button>
+      <button className="btn btn-ghost text-sm font-normal">Home</button>
+    </div>
+  )
+}
+
+export default Navbar
