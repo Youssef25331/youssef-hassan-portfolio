44fd06ebf31d361544f501de8c6153910995b9d3
Youssef25331 <yousseft25331@gmail.com>
2025-06-29 23:40:41 +0300

    even more changes to the hero

--- src/app/components/Hero/index.tsx
+++ src/app/components/Hero/index.tsx
@@ -6,8 +6,15 @@ export default function Hero() {
   return (
     <div className="h-screen w-full  flex content-center overflow-visible">
       <div className="absolute -z-1 h-full w-full ">
+        {/* <div className="absolute inset-0 bg-[radial-gradient(circle_at_30%_0%,#021318_0%,#0b0b0b_60%,transparent_100%)] " /> */}
+        {/* <div className="absolute inset-0 bg-[radial-gradient(circle_at_90%_100%,#021318_0%,#0b0b0b_100%,transparent_100%)] " /> */}
         <div className="absolute inset-0 bg-[radial-gradient(circle_at_50%_10%,#021318_0%,#0b0b0b_70%,transparent_100%)] " />
-        {/* <div className="absolute inset-0 bg-[radial-gradient(circle_at_50%_10%,#1b1b1b_0%,#2b2b2b_10%,transparent_100%)] " /> */}
+        {/* <div */}
+        {/*   className="z-[2] absolute inset-0 pointer-events-none isolate opacity-30 contain-strict hidden lg:block"> */}
+        {/*   <div className="w-[35rem] h-[80rem] -translate-y-[350px] absolute left-0 top-0 -rotate-45 rounded-full bg-[radial-gradient(68.54%_68.72%_at_55.02%_31.46%,hsla(0,0%,85%,.08)_0,hsla(0,0%,55%,.02)_50%,hsla(0,0%,45%,0)_80%)]" /> */}
+        {/*   <div className="h-[80rem] absolute left-0 top-0 w-56 -rotate-45 rounded-full bg-[radial-gradient(50%_50%_at_50%_50%,hsla(0,0%,85%,.06)_0,hsla(0,0%,45%,.02)_80%,transparent_100%)] [translate:5%_-50%]" /> */}
+        {/*   <div className="h-[80rem] -translate-y-[350px] absolute left-0 top-0 w-56 -rotate-45 bg-[radial-gradient(50%_50%_at_50%_50%,hsla(0,0%,85%,.04)_0,hsla(0,0%,45%,.02)_80%,transparent_100%)]" /> */}
+        {/* </div> */}
       </div>
       <Sparkles
         density={1200}
@@ -15,8 +22,9 @@ export default function Hero() {
         color={"#FFF"}
       ></Sparkles>
       <div className="mx-auto mt-55 w-full max-w-4xl">
-        <div className="mb-5 mt-5 flex justify-center text-center">
-          <p className='border border-base-200 rounded-4xl text-shadow-none shadow-none font-thin bg-white/10 overflow-hidden h-fit px-4 py-1 z-100 text-white/70'>
+        <div className="mb-6 mt-5 flex justify-center text-center">
+          <p className='border border-base-200 rounded-4xl  shadow-none  bg-white/10 overflow-hidden h-fit px-4 py-0.5 z-100 text-white backdrop-blur-lg text-shadow-main '>
+
             welcome to my website
           </p>
         </div>
@@ -26,7 +34,7 @@ export default function Hero() {
           </span>
           <br />
           <div className="text-6xl font-bold mt-3">
-            beautiful digital experiences
+            Beautiful digital experiences
           </div>
           <div className="mt-6">
             <span className="text-white/70 text-xl">
