960f89130dc0e10fdd4e856e145bbc0f477b4f4b
Youssef25331 <yousseft25331@gmail.com>
2025-07-07 17:10:26 +0300

    testing scroll to with lenis

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,42 +8,56 @@ import Widgets from "./components/Widgets";
 import Showcase from "./components/Showcase";
 import About from "./components/About";
 import Footer from "./components/Footer";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import Lenis from "lenis";
 
 
 export default function Home() {
 
+  const lenisRef = useRef<Lenis>(null);
+
   useEffect(() => {
     const lenis = new Lenis
+    lenisRef.current = lenis
     function raf(time: any) {
       lenis.raf(time * 0.8)
       requestAnimationFrame(raf)
     }
     requestAnimationFrame(raf)
   }, [])
+
+  const scrollTo = (target: any) => {
+    if (lenisRef.current) {
+      lenisRef.current.scrollTo(target,{duration:2,offset:-80});
+    }
+  };
+
   return (
     <>
       <NavBarLoader />
-      {/* <SmoothScroller> */}
       <main className="flex flex-col gap-10">
-        <Hero />
-        <Stack />
+        <div id="hero">
+          <Hero />
+        </div>
+        <div id="stack">
+          <Stack />
+        </div>
         <div className="my-15">
           <Widgets />
         </div>
-
-        <div className="my-15">
+        <div className="my-10" id="about">
           <About />
         </div>
-
         <div className="my-15">
           <Showcase />
         </div>
-
-        <Footer />
+        <div id="footer">
+          <Footer />
+        </div>
+        <button onClick={() => scrollTo('#about')} className="px-4 py-2 bg-blue-500 text-white rounded btn">
+          Go to About
+        </button>
       </main>
-      {/* </SmoothScroller> */}
     </>
   );
 }
