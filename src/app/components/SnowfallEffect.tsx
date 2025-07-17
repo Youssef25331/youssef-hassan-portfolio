eb9900e7e534fb4f06bbef7fa1d74260827527ec
Youssef25331 <yousseft25331@gmail.com>
2025-07-17 11:54:00 +0300

    changes to scroll

--- src/app/components/SnowfallEffect.tsx
+++ src/app/components/SnowfallEffect.tsx
@@ -4,8 +4,8 @@ import Snowfall from 'react-snowfall'
 
 const SnowfallEffect = () => {
   return (
-    <div className="w-[100%] h-[100%] fixed -z-1">
-      <Snowfall wind={[0, 0]} radius={[0.4, 0.1]} snowflakeCount={60} speed={[1, 2]} />
+    <div className="w-full h-full fixed -z-1">
+      <Snowfall wind={[0, 0]} radius={[0.4, 0.1]} snowflakeCount={60} speed={[1, 2]} style={{ display: 'flex', width: '100vw' }} />
     </div>
   )
 }
